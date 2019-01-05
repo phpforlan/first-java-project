57dbb113fa0d4447b83e61dff0ba695dc15f7bbb
lurenzhong <lurenzhong@didichuxing.com>
2019-01-05 20:47:14 +0800

    文档优化

--- imooc/src/com/imooc/HelloWorld.java
+++ imooc/src/com/imooc/HelloWorld.java
@@ -1,10 +1,29 @@
 package com.imooc;
 
+/**
+ * @author lurenzhong@didichuxing.com
+ * @version 1.0
+ * @see HelloWorld
+ */
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("hello world! happy");
+    }
+
+
+    /**
+     * 查询用户列表
+     *
+     * @param page     第几页
+     * @param pageSize 每页大小
+     * @return 返回用户列表
+     * @throws RuntimeException,Exception
+     */
+    public String queryUserList(int page, int pageSize) {
+
+        return "userList";
     }
 
 }
